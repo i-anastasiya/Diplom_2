6a66d5e22535dc2ace7cb8fccaf80de27cbd3199
Nastya Ivchenko <ivchenko.nastya@yandex.ru>
2024-08-17 00:46:10 +0300

    Задание 2: API

--- tests/test_changing_user_data.py
+++ tests/test_changing_user_data.py
@@ -14,6 +14,7 @@ class TestChangingUserData:
         new_name = {"name": new_name_value}
         rqst = requests.patch(Urls.USER, json=new_name, headers=headers)
         assert rqst.status_code == 401 and Text.AUTHORIZED in rqst.text
+
     @allure.description("Изменение поля имени, если пользователь авторизован")
     def test_auth_change_name_authorizede_user(self, create_user):
         response_created, data = create_user
@@ -23,6 +24,7 @@ class TestChangingUserData:
         new_name = {"name": new_name_value}
         rqst = requests.patch(Urls.USER, json=new_name, headers=headers)
         assert rqst.status_code == 200 and rqst.json()['user']['name'] == new_name['name']
+
     @allure.description("Изменение поля email если пользователь авторизован")
     def test_auth_change_email_authorized_user(self, create_user):
         respons_created, data = create_user
@@ -32,6 +34,7 @@ class TestChangingUserData:
         new_email = {"email": new_email_value}
         rqst = requests.patch(Urls.USER, json=new_email, headers=headers)
         assert rqst.status_code == 200 and rqst.json()['user']['email'] == new_email['email']
+
     @allure.description("Изменение поля пароль, если пользователь авторизован")
     def test_auth_change_password_authorized_user(self, create_user):
         respons_created, data = create_user
